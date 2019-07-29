ec8e32b2fdb7e9e5b56c0d99998a767ffbe7c1ef
nasrina081 <53440444+nasrina081@users.noreply.github.com>
2019-07-29 12:42:05 -0400

    Add files via upload

---
+++ day 1 coding.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#Hello my name is Nasrin
+
+print("hello my name is nasrin)
+
+5/2
+5*2
+5**2
+8/9*3
+(print(5/2))
+print(5*2,5/2,5-2,5**2,8/9*2)
---
+++ nasrins file.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+Hello my name is Nasrin
+
+print("hello my name is nasrin")
+5/2
+5*2
+5**2
+8/9*3
