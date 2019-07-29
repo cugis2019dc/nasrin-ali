58c467d1ffec5c683c09595fcd6d659630229bdc
nasrina081 <53440444+nasrina081@users.noreply.github.com>
2019-07-29 13:56:29 -0400

    Add files via upload

--- day 1 coding.py
+++ day 1 coding.py
@@ -6,7 +6,7 @@ This is a temporary script file.
 """
 #Hello my name is Nasrin
 
-print("hello my name is nasrin)
+print("hello my name is nasrin")
 
 5/2
 5*2
@@ -14,3 +14,40 @@ print("hello my name is nasrin)
 8/9*3
 (print(5/2))
 print(5*2,5/2,5-2,5**2,8/9*2)
+23/2
+
+def multiply(a,b):
+    multiply = a*b
+    
+    print(multiply)
+    
+multiply(2,5)
+
+
+def sum(a,b,c):
+    sum=a+b+c
+    print(sum)
+    
+sum(1,2,3)
+
+def sum(a,b,c):
+    sum=a+b+c
+    print(sum)
+    
+sum(2,4,5)
+
+def area(a,c,d):
+    area = a*c*d
+    
+    print("the area of the triangle with base 6 and height 3 is",area)
+    
+area(1/2,6,3)
+    
+def triarea(b,h):
+    triarea = 1/2*b*h
+    print('the area of the triangle with base' b,'and height ',h, 'is' triarea)
+    
+triarea(6,3)
+    
+    
+    
